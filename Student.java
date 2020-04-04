359f59d043545c0cb78eb555bfa381cf2adf6dae
david-todorov-student <todorov_david@yahoo.com>
2020-04-04 16:57:33 -0700

    student class finished

--- Student.java
+++ Student.java
@@ -4,14 +4,52 @@ class Student {
     String lastName;
     List<Integer> grades;
     //TODO constructor
-	
-    //TODO seters & getters
 
-    public double getAverage() {
-        //TODO
+	public Student(String index, String firstName, String lastName) {
+		this.index = index;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.grades = new ArrayList<>();
+	}
+
+	//TODO seters & getters
+
+	public String getIndex() {
+		return index;
+	}
+
+	public void setIndex(String index) {
+		this.index = index;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public double getAverage() {
+		//TODO
+		return this.grades.stream()
+				.mapToDouble(i -> i.doubleValue())
+				.average()
+				.orElse(0);
     }
 
     public int ECTSCredits() {
         //TODO
+		return 6*this.grades.stream()
+				.filter(i->i>=6)
+				.count();
     }
 }
